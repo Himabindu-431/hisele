9ac30db18d6c9e5aae39553631d6788e4715c900
Arjun <Arjun@DESKTOP-68228J9>
2020-02-21 18:11:19 +0530

    wbskv

--- src/ppppppp.java
+++ src/ppppppp.java
@@ -1,7 +1,7 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class oooooo {
+public class ppppppp {
 	public static void main(String[] args) throws InterruptedException {
 		System.setProperty("webdriver.chrome.driver", ".\\softwares\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
